72e2527b9a503b880735769b7451f183e0985adc
Daniel Król <danielkrol90@gmail.com>
2022-08-26 15:04:29 +0200

    stuck on js code with open/close icon toogle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// change navbar style on scroll
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = document.querySelector('.faq-icon i');
+        if (icon.className === "bi bi-plus-circle") {
+            icon.className = "bi bi-dash-circle"
+        } else {
+            icon.className = "bi bi-plus-circle"
+        }
+    })
 })
