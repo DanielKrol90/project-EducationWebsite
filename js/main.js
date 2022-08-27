cb16406c7c8e097696a347119a16a68d1c7d27f6
Daniel Król <danielkrol90@gmail.com>
2022-08-27 08:15:51 +0200

    mediaQ for mobile

--- js/main.js
+++ js/main.js
@@ -1,41 +1,51 @@
 // change navbar style on scroll
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
 
 // show/hide faq answer
 
-const faqs = document.querySelectorAll('.faq');
+const faqs = document.querySelectorAll(".faq");
 
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
 
-        // change icon
-        const icon = faq.querySelector('.faq-icon i');
-        if (icon.className === "bi bi-plus-circle") {
-            icon.className = "bi bi-dash-circle"
-        } else {
-            icon.className = "bi bi-plus-circle"
-        }
-    })
-})
-
-// swiper testimonials 
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-        600: {
-            slidesPerView: 2
-        }
+    // change icon
+    const icon = faq.querySelector(".faq-icon i");
+    if (icon.className === "bi bi-plus-circle") {
+      icon.className = "bi bi-dash-circle";
+    } else {
+      icon.className = "bi bi-plus-circle";
     }
   });
+});
+
+// swiper testimonials
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 600px
+    600: {
+      slidesPerView: 2,
+    },
+    992: {
+        slidesPerView: 3,
+      },
+  },
+});
 
 //   show/open nav menu mobile
 
@@ -43,18 +53,18 @@ const menu = document.querySelector(".nav-menu");
 const menuOpenBtn = document.querySelector("#open-menu-btn");
 const menuCloseBtn = document.querySelector("#close-menu-btn");
 
-menuOpenBtn.addEventListener("click",() => {
-    menu.style.display = "flex";
-    menuCloseBtn.style.display = "inline-block";
-    menuOpenBtn.style.display = "none";
+menuOpenBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  menuCloseBtn.style.display = "inline-block";
+  menuOpenBtn.style.display = "none";
 });
 
 // close nav menu mobile
 
 const closeNavMobile = () => {
-    menu.style.display = "none";
-    menuCloseBtn.style.display = "none";
-    menuOpenBtn.style.display = "inline-block";
-}
+  menu.style.display = "none";
+  menuCloseBtn.style.display = "none";
+  menuOpenBtn.style.display = "inline-block";
+};
 
 menuCloseBtn.addEventListener("click", closeNavMobile);
